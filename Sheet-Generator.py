5deb59455be82940706118f093fc54d6db2cea7f
Ahmed Hossam <ahmed.7oskaa@gmail.com>
2022-04-03 16:28:01 +0200

    Initialize Commit

--- Sheet-Generator.py
+++ Sheet-Generator.py
@@ -12,11 +12,6 @@ load_dotenv()
 red = '\033[38;5;196m'
 green = '\033[38;5;40m'
 blue = '\033[34m'
-gold = '\033[38;5;220m'
-white = '\33[37m'
-magenta = '\033[35m'
-bold = '\033[01m'
-reset = '\033[0m'
 
 
 # To make the problem in the valid format for being added to codeforces
