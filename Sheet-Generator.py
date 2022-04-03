b58cd8e22d33559d3c2b0bbc9237315200cd7987
Ahmed Hossam <ahmed.7oskaa@gmail.com>
2022-04-03 16:09:49 +0200

    Initialize Commit

--- Sheet-Generator.py
+++ Sheet-Generator.py
@@ -2,7 +2,6 @@ import os
 import time
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
