6c4bc6ea08f686d9449d6c9e58aba41dcd1f4b5c
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-07-31 22:00:02 +0200

    make the Generator compitable with codeforces

---
+++ Generator.cpp
@@ -0,0 +1,213 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define all(vec) (vec.begin(), vec.end())
+#define sz(x) (int) x.size()
+
+const int N = 1e6 + 4;
+
+int32_t permutation[N];
+
+const ll INF = 1LL << 60;
+
+using pii = pair < ll, ll >;
+
+namespace generator {
+
+	ll gen_int(ll l = -INF, ll r = INF){
+        return rnd.next(l, r);
+	}
+
+    string gen_string(int len = 0, bool upperCase = false, ll l = 1, ll r = 26) {
+        assert(len >= 0 && len <= 5e6);
+        string str(len, (upperCase ? 'A' : 'a'));
+        for (char &ch: str)
+            ch += gen_int(l, r) - 1;
+        return str;
+    }
+
+    string gen_string_u_and_l(int len = 0, char l = 'a', char r = 'z'){
+        assert(len >= 0 && len <= 5e6);
+        string str(len, 'A');
+        for(auto& ch : str){
+            bool is_upper = gen_int(0, 1);
+            if(is_upper)
+                ch = 'A' + gen_int(l - 'A', r - 'A');
+            else
+                ch = 'a' + gen_int(l - 'a', r - 'A');
+        }
+        return str;
+    }
+
+    string gen_palindrome(int len = 0, bool upperCase = false, char l = 'a', char r = 'z'){
+        assert(len >= 0 && len <= 5e6);
+        string str(len, (upperCase ? 'A' : 'a'));
+        for (int left = 0, right = len - 1; left <= right; left++, right--)
+            str[left] = str[right] = str[left] + gen_int(l - (upperCase ? 'A' : 'a'), r - (upperCase ? 'A' : 'a'));
+        return str;
+    }
+
+    vector < ll > gen_array(int len = 0, ll minRange = -INF, ll maxRange = INF, bool Increasing = false, bool Decreasing = false){
+        assert(len >= 0 and len <= 5e6);
+        vector < ll > vec(len);
+        ll Delta = max(maxRange / len, 1ll);
+        for (auto &x: vec) x = gen_int(minRange, maxRange);
+        if(Increasing){
+            for(int i = 0; i < len; i++)
+                vec[i] = (i == 0 ? 0 : vec[i - 1]) + Delta;
+        }
+        if(Decreasing){
+            for(int i = len - 1; i >= 0; i--)
+                vec[i] = (i == len - 1 ? 0 : vec[i + 1]) + Delta;
+        }
+        return vec;
+    }
+
+    vector < ll > gen_unique_array(int len = 0, ll minRange = -INF, ll maxRange = INF, bool Increasing = false, bool Decreasing = false){
+        assert(len >= 0 and len <= 5e6);
+        vector < ll > vec;
+        set < ll > st;
+        while(sz(st) != len)
+            st.insert(gen_int(minRange, maxRange));
+        for(auto& i : st)
+            vec.push_back(i);
+        if(Increasing) return vec;
+        if(Decreasing) {
+            reverse(vec.begin(), vec.end());
+            return vec;
+        }
+        shuffle(vec.begin(), vec.end());
+        return vec;
+    }
+
+    vector < vector < ll > > gen_array_2D(int row = 0, int col = 0, ll minRange = -INF, ll maxRange = INF){
+		assert(row >= 0 and row <= 5e6 && col >= 0 && col <= 5e6 && row * col <= 1e8);
+		vector < vector < ll > > vec(row);
+		for(int i = 0; i < row; i++)
+			vec[i] = gen_array(col);
+		return vec;
+	}
+
+    vector < ll > gen_permutation(int len = 0, ll minRange = -INF, ll maxRange = INF){
+        assert(len >= 0 && len <= 5e6);
+        vector < ll > vec(len);
+        iota(vec.begin(), vec.end(), 1);
+        shuffle(vec.begin(), vec.end());
+        return vec;
+    }
+
+    string gen_big_int(int len = 0, char l = '0', char r = '9'){
+		assert(len >= 0 && len <= 5e6);
+		string str(len, '0');
+		for (char &ch: str) 
+			ch += gen_int(l - '0', r - '0');
+		if(str.front() == '0') str.front() += gen_int(l - '0' + 1, r - '0');
+		return str;
+  	}
+
+    vector < pii > gen_array_of_pairs(int len = 0, ll minRange = -INF, ll maxRange = INF, bool Interval = false){
+        assert(len >= 0 and len <= 5e6);
+        vector < pii > vec(len);
+        for (auto &[l, r]: vec) l = gen_int(minRange, maxRange), r = gen_int((Interval ? l : minRange), maxRange);
+        return vec;
+    }
+
+    char gen_char(bool upperCase = false, bool Digit = false, char l = 'a', char r = 'z'){
+		if(upperCase)
+			return 'A' + gen_int(l - 'A', r - 'A');
+		if(Digit)
+			return '0' + gen_int(l - '0', r - '0');
+		return 'a' + gen_int(l - 'a', r - 'a');
+	}
+
+    vector < pii > gen_tree(ll n = 0){
+        assert(n >= 0);
+        vector < pii > res(n ? n - 1 : 0);
+        // if you like to have bamboo like tree or star like tree uncomment below 8 lines
+        /*if (rng() % 5 == 0) { // bamboo like tree
+            for (int i = 1; i < n; ++i) res[i-1] = {i, i + 1};
+            return res;
+        }
+        if (rng() % 7 == 0) { // star tree
+            for (int i = 2; i <= n; ++i) res[i-2] = {1, i};
+            return res;
+        }*/
+        iota(permutation, permutation + 1 + n, 0);
+        shuffle(permutation + 1, permutation + 1 + n);
+        for(int i = 2; i <= n; ++i){
+            ll u = i, v = gen_int(1 , i - 1);
+            u = permutation[u], v = permutation[v];
+            res[i - 2] = minmax(u, v); // u < v, just for convenience while debugging
+        }
+        shuffle(res.begin(), res.end());
+        return res;
+    }
+  
+    vector < pii > simple_graph(ll n = 0, ll m = 0) {
+        assert(n > 0 && m >= n);
+        ll max_edges = n * (n - 1) / 2;
+        assert(m <= max_edges);
+        vector < pii > res = gen_tree(n);
+        set < pii > edge(res.begin(), res.end());
+        for (int i = n; i <= m; ++i) {
+            while (true) {
+                ll u = gen_int(1, n), v = gen_int(1, n);
+                if (u == v) continue;
+                auto it = edge.insert(minmax(u, v));
+                if (it.second) break;
+            }
+        }
+        res.assign(edge.begin(), edge.end());
+        return res;
+    }
+
+}
+
+using namespace generator;
+
+template < typename T = int > ostream& operator << (ostream &other, const vector<T> &v) {
+    for (const T &x: v) other << x << ' ';
+    return other;
+}
+
+template < typename T = int > ostream& operator << (ostream &other, const vector < pair < T, T > > &v) {
+    for (const auto &x : v) other << x.first << ' ' << x.second << '\n';
+    return other;
+}
+
+// comment the just below line if test cases required
+#define SINGLE_TEST
+constexpr int Tests = 20, Sum_of_Tests = 2e5;
+
+// complete this function according to the requirements
+void Generate_test() {
+
+}
+// make name of text file
+// freopen(made_txt(f).c_str(), "w", stdout);
+string made_txt(int t){
+    string testcase = "Tests/test";
+    testcase += to_string(t);
+    testcase += ".txt";
+    return testcase;
+}
+
+// use generator in Codeforces Polygon
+/*
+    <#list 1..30 as s>
+        Generator ${s} > $
+    </#list>
+*/
+
+int main(int argc, char* argv[]){
+    registerGen(argc, argv, 1);
+    int T = 1;
+    #ifndef SINGLE_TEST
+        T = gen_int(1, Tests), cout << T << '\n';
+    #endif
+    for(int t = 1; t <= T; t++)
+        Generate_test();
+}
--- README.md
+++ README.md
@@ -1,23 +1,109 @@
-# Codeforces-Sheet-Generator
+# Codeforces-Polygon-Template
 
-It's a simple script to generate a mushup on code forces, the script will accept the public problem urls only or polygon problems.
+### Template for codeforces contest preperation on [polygon](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiU3cXDq6b2AhWQ-6QKHY7YAX0QFnoECAoQAQ&url=https%3A%2F%2Fpolygon.codeforces.com%2F&usg=AOvVaw3i2v31m8II47sBuBT85c8t)
 
-## Requirements
+---
 
-- Copy the `.env.example` file and rename it to `.env` and provide the required environment variables.
-  - CODEFORCES_HANDLE
-  - CODEFORCES_PASSWORD
-  - SHEET_NAME
-  - SHEET_DURATION
-- Copy the urls of the problems to `Problems_Links.txt`
+There are 4 files to use
 
-```bash
-# install requirements
-pip3 install -r requirements.txt
-```
+- Validator.cpp
+  - to make validator for the problem to check the validation of the test case
 
-## Running the script
+- Checker.cpp
+  - to make special checker for your problem when there a lot of valid solutions
 
-```bash
-python3 Sheet_Generator.py
-```
+- Solution.cpp
+  - to put your solution in it and upload it
+
+- Generator.cpp
+  - to generate the test cases that you need in your problem
+
+`input.txt` and `output.txt` we use them with `Solution.cpp` file but you can ignore them and use your terminal instead of files
+
+---
+
+There is folder `testlib-master` in it there are a lot of examples of checkers, generators, validators, and there is the header file that we include it in validator.cpp and checker.cpp `testlib.h`
+
+**Linux**:
+
+- **You should copy to `testlib.h` and `testlib.h.gch` from `testlib-master` to `/usr/local/include`**
+
+**Windows**:
+
+- **You should copy to `testlib.h` and `testlib.h.gch` from `testlib-master` to `include` folder in your compiler folder**
+
+---
+
+You can read information about `testlib.h` from [here](https://github.com/7oSkaaa/Codeforces-Polygon-Template/blob/main/testlib-master/README.md).
+
+You can see these tutorial, they will help you to write a good problem
+
+- [Writing Statements tutorial](https://polygon.codeforces.com/docs/statements-tex-manual?ccid=f5bddf71bb1644157d570cc2043d9df7&session=68e51869dd74479dbd85d7d89b67012852f5a418)
+
+- [Validators tutorial](https://codeforces.com/blog/entry/18426)
+
+- [Checker tutorial](https://codeforces.com/blog/entry/18431)
+
+- [Generators tutorial](https://codeforces.com/blog/entry/18291)
+
+- [Interactors tutorial](https://codeforces.com/blog/entry/18455)
+
+- [testlib.h information](https://codeforces.com/blog/entry/18289)
+
+---
+
+There are generator builtin-functions you can use from `testlib.h`, and you can use `Generator.cpp` file.
+
+If you will use `Generator.cpp` file, these information will help you to use it.
+
+- Choose number of file you want to print in line `134`
+- Choose number of test cases in each file from line `131`
+- If your problem has a single test case uncomment line `130`
+
+there are many functions to help you to generate test like:
+
+- gen_string
+  - generate a string `Upper Case` or `Lower Case` and in any range you want with your length
+
+- gen_string_u_and_l
+  - generate a string `Upper Case` and `Lower Case` and in any range you want with your length
+ 
+- gen_int
+  - generate a integer number in the range you want from `L` to `R`
+
+- gen_palindrome
+  - generate a palindrome string with your length in the range that you want
+
+- gen_array
+  - generate an array with your size and in range you want, you can make the array `normal` or `increasing` or `decreasing`
+
+- gen_array_2D
+  - generate an array_2D with your size and in range you want.
+
+- gen_permutation
+  - generate an array with a permutation of `N`
+
+- gen_bit_int
+  - generate a string with your length consit of digit in the range that you want
+
+- gen_array_of_pairs
+  - generate an array of pairs with your length in the range that you want, you can choose if the pair is interval or not.
+
+- gen_char
+  - gen character in range that you want 
+
+- gen_tree
+  - generate a tree or `bamboo` like tree or `star` like tree
+
+- gen_graph
+  - generate a simple graph
+
+The test cases files will exported in `Tests` folder in the same directory of the `Generator.cpp` file
+
+---
+
+### You can see tutorial video for polygon site, how to use it and write a problem live from [](https://drive.google.com/file/d/198dWogj2xSGWZxVMNtCRMqVnawgVRXLX/view?usp=sharing)
+
+---
+
+#### This a tutorial for polygon and how to use it [Ali Ibrahim Site](https://ali-ibrahim137.github.io/competitive/programming/2020/09/27/polygon-codeforces.html)
